bbd9359742052255cd5a4e662fef42165441c708
Anastasiia Dali <anastasiia.dalakishvili@gmail.com>
2022-05-18 18:01:46 +0100

    refactor number of iterations

--- merge_lists/main.go
+++ merge_lists/main.go
@@ -5,13 +5,14 @@ import "fmt"
 func main() {
 	arr1 := []int{1, 4, 6}
 	arr2 := []int{2, 3, 5}
-	fmt.Print("Success ", merge(arr1, arr2))
+	fmt.Print("Success ", Merge(arr1, arr2))
 }
 
-func merge(arr1 []int, arr2 []int) []int {
+func Merge(arr1 []int, arr2 []int) []int {
 	var mergedArr []int
+	numberOfIteration := len(arr1) + len(arr2) - 1
 
-	for i := 0; i < len(arr1)+len(arr2)+4; i++ {
+	for i := 0; i < numberOfIteration; i++ {
 		if arr1[0] > arr2[0] {
 			mergedArr = append(mergedArr, arr2[0])
 			arr2 = append(arr2[:0], arr2[1:]...)
