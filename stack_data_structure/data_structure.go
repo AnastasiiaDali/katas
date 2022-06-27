106b8b956461e358eb03eb397c4e91030af89e2e
Anastasiia Dali <anastasiia.dalakishvili@gmail.com>
2022-06-27 21:59:21 +0100

    data structure kata

---
+++ stack_data_structure/data_structure.go
@@ -0,0 +1,52 @@
+package stack_data_structure
+
+import "log"
+
+type Stack struct {
+	stack       []int
+	lastElement int
+}
+
+func NewStack(capacity int) *Stack {
+	return &Stack{
+		stack:       make([]int, capacity),
+		lastElement: -1,
+	}
+}
+
+func (s *Stack) Push(item int) bool {
+	if s.IsFull() {
+		log.Print("stack is full, can not push new item")
+		return false
+	}
+	s.lastElement += 1
+	s.stack[s.lastElement] = item
+	return true
+}
+
+func (s *Stack) Pop() (int, bool) {
+	if s.IsEmpty() {
+		log.Print("stack is empty, nothing to pop")
+		return 0, false
+	}
+	popItem := s.stack[s.lastElement]
+	s.stack = s.stack[:popItem]
+	return popItem, true
+}
+
+func (s Stack) Peek() (int, bool) {
+	if s.IsEmpty() {
+		log.Print("stack is empty, nothing to peek")
+		return 0, false
+	}
+	peekedItem := s.stack[s.lastElement]
+	return peekedItem, true
+}
+
+func (s *Stack) IsEmpty() bool {
+	return s.lastElement == -1
+}
+
+func (s *Stack) IsFull() bool {
+	return s.lastElement == len(s.stack)-1
+}
---
+++ stack_data_structure/data_structure_test.go
@@ -0,0 +1,143 @@
+package stack_data_structure_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"merge_lists/stack_data_structure"
+)
+
+func TestStackData(t *testing.T) {
+	t.Run("NewStack should return empty stack", func(t *testing.T) {
+		//arrange
+		capacity := 10
+
+		//act
+		stack := stack_data_structure.NewStack(capacity)
+
+		//assert
+		assert.True(t, stack.IsEmpty())
+	})
+	t.Run("testing Push", func(t *testing.T) {
+		t.Run("Push should return a stack with an item", func(t *testing.T) {
+			//arrange
+			item := 1
+			capacity := 5
+
+			stack := stack_data_structure.NewStack(capacity)
+
+			//act
+			stack.Push(item)
+
+			//assert
+			assert.False(t, stack.IsEmpty())
+		})
+		t.Run("Push should return false if stack is already full", func(t *testing.T) {
+			//arrange
+			item1, item2, item3 := 1, 2, 3
+			capacity := 2
+
+			stack := stack_data_structure.NewStack(capacity)
+
+			stack.Push(item1)
+			stack.Push(item2)
+
+			//act
+			boolValue := stack.Push(item3)
+
+			//assert
+			assert.False(t, boolValue)
+		})
+	})
+
+	t.Run("testing Peek", func(t *testing.T) {
+		t.Run("Peek should return the last item from the stuck but not remove it", func(t *testing.T) {
+			//arrange
+			item1, item2 := 1, 2
+			capacity := 5
+
+			stack := stack_data_structure.NewStack(capacity)
+
+			stack.Push(item1)
+			stack.Push(item2)
+
+			//act
+			peekedItem, _ := stack.Peek()
+
+			//assert
+			assert.Equal(t, 2, peekedItem)
+			assert.False(t, stack.IsEmpty())
+		})
+		t.Run("Peek should return false if stack is empty", func(t *testing.T) {
+			//arrange
+			capacity := 5
+			stack := stack_data_structure.NewStack(capacity)
+
+			//act
+			_, boolValue := stack.Peek()
+
+			//assert
+			assert.False(t, false, boolValue)
+		})
+	})
+
+	t.Run("testing IsEmpty", func(t *testing.T) {
+		t.Run("IsEmpty should return true when stack is empty", func(t *testing.T) {
+			//arrange
+			capacity := 5
+			stack := stack_data_structure.NewStack(capacity)
+
+			//act
+			boolValue := stack.IsEmpty()
+
+			//assert
+			assert.True(t, boolValue)
+		})
+		t.Run("IsEmpty should return false when stack is NOT empty", func(t *testing.T) {
+			//arrange
+			item1 := 1
+			capacity := 3
+
+			stack := stack_data_structure.NewStack(capacity)
+			stack.Push(item1)
+
+			//act
+			boolValue := stack.IsEmpty()
+
+			//assert
+			assert.False(t, boolValue)
+		})
+	})
+	t.Run("testing IsFull", func(t *testing.T) {
+		t.Run("IsFull should return true when stack is full", func(t *testing.T) {
+			//arrange
+			item1, item2 := 1, 2
+			capacity := 2
+			stack := stack_data_structure.NewStack(capacity)
+
+			stack.Push(item1)
+			stack.Push(item2)
+
+			//act
+			boolValue := stack.IsFull()
+
+			//assert
+			assert.True(t, boolValue)
+		})
+		t.Run("IsFull should return false when stack is NOT full", func(t *testing.T) {
+			//arrange
+			item1 := 1
+			capacity := 3
+
+			stack := stack_data_structure.NewStack(capacity)
+			stack.Push(item1)
+
+			//act
+			boolValue := stack.IsFull()
+
+			//assert
+			assert.False(t, boolValue)
+		})
+	})
+
+}
