440ae6b8a03ade1c265e318f6f4f198e925b058b
Anastasiia Dali <anastasiia.dalakishvili@gmail.com>
2022-07-03 16:15:38 +0100

    perfect spherical houses in vacuum part 1

--- perfectly_spherical_houses_in_a_vacuum/perfectly_spherical_houses_in_a_vacuum.go
+++ perfectly_spherical_houses_in_a_vacuum/perfectly_spherical_houses_in_a_vacuum.go
@@ -1,5 +1,65 @@
 package perfectly_spherical_houses_in_a_vacuum
 
+import (
+	"strings"
+
+	"golang.org/x/exp/slices"
+)
+
 func HouseCounter(direction string) int {
-	return 0
+	//input: "^>v<", want: 4
+	// ^ = y + 1
+	// v = y - 1
+	// > = x + 1
+	// < = x - 1
+
+	coordinatesXY := [][]int{{0, 0}}
+
+	houses := 1
+
+	directions := strings.Split(direction, "")
+	for _, dir := range directions {
+		lastCoordinates := coordinatesXY[len(coordinatesXY)-1]
+
+		switch dir {
+		case "^":
+			if contains(coordinatesXY, []int{lastCoordinates[0], lastCoordinates[1] + 1}) {
+				coordinatesXY = append(coordinatesXY, []int{lastCoordinates[0], lastCoordinates[1] + 1})
+			} else {
+				coordinatesXY = append(coordinatesXY, []int{lastCoordinates[0], lastCoordinates[1] + 1})
+				houses += 1
+			}
+		case "v":
+			if contains(coordinatesXY, []int{lastCoordinates[0], lastCoordinates[1] - 1}) {
+				coordinatesXY = append(coordinatesXY, []int{lastCoordinates[0], lastCoordinates[1] - 1})
+			} else {
+				coordinatesXY = append(coordinatesXY, []int{lastCoordinates[0], lastCoordinates[1] - 1})
+				houses += 1
+			}
+		case ">":
+			if contains(coordinatesXY, []int{lastCoordinates[0] + 1, lastCoordinates[1]}) {
+				coordinatesXY = append(coordinatesXY, []int{lastCoordinates[0] + 1, lastCoordinates[1]})
+			} else {
+				coordinatesXY = append(coordinatesXY, []int{lastCoordinates[0] + 1, lastCoordinates[1]})
+				houses += 1
+			}
+		case "<":
+			if contains(coordinatesXY, []int{lastCoordinates[0] - 1, lastCoordinates[1]}) {
+				coordinatesXY = append(coordinatesXY, []int{lastCoordinates[0] - 1, lastCoordinates[1]})
+			} else {
+				coordinatesXY = append(coordinatesXY, []int{lastCoordinates[0] - 1, lastCoordinates[1]})
+				houses += 1
+			}
+		}
+	}
+	return houses
+}
+
+func contains(s [][]int, e []int) bool {
+	for _, a := range s {
+		if slices.Equal(a, e) {
+			return true
+		}
+	}
+	return false
 }
