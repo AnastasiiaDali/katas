3be53844f1b54306df22567681288295e8daa7ab
Anastasiia Dali <anastasiia.dalakishvili@gmail.com>
2022-08-10 11:02:25 +0100

    refactor kata using maps

--- perfectly_spherical_houses_in_a_vacuum_part_2/perfectly_spherical_houses_in_a_vacuum_part_2.go
+++ perfectly_spherical_houses_in_a_vacuum_part_2/perfectly_spherical_houses_in_a_vacuum_part_2.go
@@ -2,95 +2,57 @@ package perfectly_spherical_houses_in_a_vacuum_part_2
 
 import (
 	"strings"
-
-	"golang.org/x/exp/slices"
 )
 
+type Coordinates struct {
+	X int
+	Y int
+}
+
 func HouseCounter(direction string) int {
-	housesRobotSantaVisited := 1
-	housesRealSantaVisited := 0
-	coordinatesXYRobotSanta := [][]int{{0, 0}}
-	coordinatesXYRealSanta := [][]int{{0, 0}}
+	santaLocation := Coordinates{X: 0, Y: 0}
+	robotLocation := Coordinates{X: 0, Y: 0}
+
+	visitedHousesBySanta := map[Coordinates]int{}
+	visitedHousesByRobot := map[Coordinates]int{}
+
+	visitedHousesBySanta[santaLocation] = 1
+	visitedHousesByRobot[robotLocation] = 0
+
 	directions := strings.Split(direction, "")
+	if len(directions) == 0 {
+		return 0
+	}
 
 	for i, dir := range directions {
 
 		if i == 0 || i%2 == 0 {
-			lastCoordinates := coordinatesXYRobotSanta[len(coordinatesXYRobotSanta)-1]
 			switch dir {
 			case "^":
-				if contains(coordinatesXYRobotSanta, []int{lastCoordinates[0], lastCoordinates[1] + 1}) {
-					coordinatesXYRobotSanta = append(coordinatesXYRobotSanta, []int{lastCoordinates[0], lastCoordinates[1] + 1})
-				} else {
-					coordinatesXYRobotSanta = append(coordinatesXYRobotSanta, []int{lastCoordinates[0], lastCoordinates[1] + 1})
-					housesRobotSantaVisited += 1
-				}
+				santaLocation.Y += 1
 			case "v":
-				if contains(coordinatesXYRobotSanta, []int{lastCoordinates[0], lastCoordinates[1] - 1}) {
-					coordinatesXYRobotSanta = append(coordinatesXYRobotSanta, []int{lastCoordinates[0], lastCoordinates[1] - 1})
-				} else {
-					coordinatesXYRobotSanta = append(coordinatesXYRobotSanta, []int{lastCoordinates[0], lastCoordinates[1] - 1})
-					housesRobotSantaVisited += 1
-				}
+				santaLocation.Y -= 1
 			case ">":
-				if contains(coordinatesXYRobotSanta, []int{lastCoordinates[0] + 1, lastCoordinates[1]}) {
-					coordinatesXYRobotSanta = append(coordinatesXYRobotSanta, []int{lastCoordinates[0] + 1, lastCoordinates[1]})
-				} else {
-					coordinatesXYRobotSanta = append(coordinatesXYRobotSanta, []int{lastCoordinates[0] + 1, lastCoordinates[1]})
-					housesRobotSantaVisited += 1
-				}
+				santaLocation.X += 1
 			case "<":
-				if contains(coordinatesXYRobotSanta, []int{lastCoordinates[0] - 1, lastCoordinates[1]}) {
-					coordinatesXYRobotSanta = append(coordinatesXYRobotSanta, []int{lastCoordinates[0] - 1, lastCoordinates[1]})
-				} else {
-					coordinatesXYRobotSanta = append(coordinatesXYRobotSanta, []int{lastCoordinates[0] - 1, lastCoordinates[1]})
-					housesRobotSantaVisited += 1
-				}
+				santaLocation.X -= 1
 			}
+			visitedHousesBySanta[santaLocation] += 1
 		} else {
-			lastCoordinates := coordinatesXYRealSanta[len(coordinatesXYRealSanta)-1]
 			switch dir {
 			case "^":
-				if contains(coordinatesXYRealSanta, []int{lastCoordinates[0], lastCoordinates[1] + 1}) {
-					coordinatesXYRealSanta = append(coordinatesXYRealSanta, []int{lastCoordinates[0], lastCoordinates[1] + 1})
-				} else {
-					coordinatesXYRealSanta = append(coordinatesXYRealSanta, []int{lastCoordinates[0], lastCoordinates[1] + 1})
-					housesRealSantaVisited += 1
-				}
+				robotLocation.Y += 1
 			case "v":
-				if contains(coordinatesXYRealSanta, []int{lastCoordinates[0], lastCoordinates[1] - 1}) {
-					coordinatesXYRealSanta = append(coordinatesXYRealSanta, []int{lastCoordinates[0], lastCoordinates[1] - 1})
-				} else {
-					coordinatesXYRealSanta = append(coordinatesXYRealSanta, []int{lastCoordinates[0], lastCoordinates[1] - 1})
-					housesRealSantaVisited += 1
-				}
+				robotLocation.Y -= 1
 			case ">":
-				if contains(coordinatesXYRealSanta, []int{lastCoordinates[0] + 1, lastCoordinates[1]}) {
-					coordinatesXYRealSanta = append(coordinatesXYRealSanta, []int{lastCoordinates[0] + 1, lastCoordinates[1]})
-				} else {
-					coordinatesXYRealSanta = append(coordinatesXYRealSanta, []int{lastCoordinates[0] + 1, lastCoordinates[1]})
-					housesRealSantaVisited += 1
-				}
+				robotLocation.X += 1
 			case "<":
-				if contains(coordinatesXYRealSanta, []int{lastCoordinates[0] - 1, lastCoordinates[1]}) {
-					coordinatesXYRealSanta = append(coordinatesXYRealSanta, []int{lastCoordinates[0] - 1, lastCoordinates[1]})
-				} else {
-					coordinatesXYRealSanta = append(coordinatesXYRealSanta, []int{lastCoordinates[0] - 1, lastCoordinates[1]})
-					housesRealSantaVisited += 1
-				}
+				robotLocation.X -= 1
 			}
+			visitedHousesByRobot[robotLocation] += 1
 		}
 
 	}
 
-	return housesRobotSantaVisited + housesRealSantaVisited
-}
-
-func contains(s [][]int, e []int) bool {
-	for _, a := range s {
-		if slices.Equal(a, e) {
-			return true
-		}
-	}
-	return false
+	return len(visitedHousesByRobot) + len(visitedHousesBySanta) - 1
 }
